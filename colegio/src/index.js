5a88ecffd678d772d85fa1c0a2a21307461b8645
Oscar Escobar <qomc16@gmail.com>
2023-11-09 01:23:39 -0600

    Correción de codigo

--- colegio/src/index.js
+++ colegio/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import 'primeicons/primeicons.css';
+import 'primereact/resources/themes/lara-light-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
 
