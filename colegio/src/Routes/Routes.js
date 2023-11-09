fd74d43397e3875f269292281e705e3ec0b98e7e
Oscar Escobar <qomc16@gmail.com>
2023-11-09 03:11:09 -0600

    Redireccionamiento automático de las rutas

--- colegio/src/Routes/Routes.js
+++ colegio/src/Routes/Routes.js
@@ -1,14 +1,18 @@
 // Routes.js
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Dashboard from '../components/Dashboard';
 import LoginComponent from '../components/login';
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<LoginComponent/>} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/" element={<LoginComponent />} />
+      <Route path="/Dashboard" element={<Dashboard />} />
+      <Route
+        path="*"
+        element={<Navigate to="/" replace />}
+      />
     </Routes>
   );
 };
